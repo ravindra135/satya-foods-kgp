534a34112f94089b59d06ff6707437d804ba5422
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-29 11:18:56 +0530

    project completed

--- js/app.js
+++ js/app.js
@@ -9,10 +9,19 @@ $("#navClose").click(function() {
     $("#menuOverlay").toggleClass("hidden");
 });
 
+
+// Auto Typing
 let typed = new Typed('#typed', {
-    strings: ['Joy,', 'Yummy,', 'Tasty', 'Experience.'],
+    strings: ['Joy,', 'Yummy,', 'Tasty,', 'Experience.'],
     typeSpeed: 120,
     backSpeed: 75,
     fadeOut: true,
     loop: true
-  });
+});
+
+// Whatsapp Code
+let wa_btnSetting = {"btnColor":"#2bee62","ctaText":"Order On The Go","cornerRadius":40,"marginBottom":20,"marginLeft":20,"marginRight":20,"btnPosition":"left","whatsAppNumber":"918697034671","welcomeMessage":"Hey,\nI Want to Order ","zIndex":3,"btnColorScheme":"light"};
+window.onload = () => {
+  _waEmbed(wa_btnSetting);
+};
+  
