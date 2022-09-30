06d1c7131dc19fed584d299e8b74cdb014710355
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-30 09:25:29 +0530

    modifications done

--- js/app.js
+++ js/app.js
@@ -12,24 +12,16 @@ $("#navClose").click(function() {
 
 // Auto Typing
 let typed = new Typed('#typed', {
-    strings: ['Joy,', 'Yummy,', 'Tasty,', 'Experience.'],
+    strings: ['Joy,', 'Yummy,', 'Tasty,', 'Experience...'],
     typeSpeed: 125,
     backSpeed: 75,
     fadeOut: true,
     loop: true
 });
-
-// Whatsapp Code
-// let wa_btnSetting = {"btnColor":"#2bee62","ctaText":"Order On The Go","cornerRadius":40,"marginBottom":20,"marginLeft":20,"marginRight":20,"btnPosition":"left","whatsAppNumber":"918697034671","welcomeMessage":"Hey,\nI Want to Order ","zIndex":3,"btnColorScheme":"light"};
-// window.onload = () => {
-//   _waEmbed(wa_btnSetting);
-// };
   
 // Slider
-
-
-// $('#auto').bbslider({
-//     auto: true,
-//     timer: 2500,
-//     loop: true
-// });
+$('#auto').bbslider({
+    auto: true,
+    timer: 2500,
+    loop: true
+});
